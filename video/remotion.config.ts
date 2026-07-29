9462acde26b8ba7c3915986d66dc237678f05cec
Victor <alimurtazadhuddi@gmail.com>
2026-07-29 23:47:11 +0500

    check

--- video/remotion.config.ts
+++
@@ -1,6 +0,0 @@
-import { Config } from '@remotion/cli/config';
-
-Config.setVideoImageFormat('jpeg');
-Config.setOverwriteOutput(true);
-// Crisper text/edges than the default 1x when rendering the 4K variant.
-Config.setChromiumOpenGlRenderer('angle');
